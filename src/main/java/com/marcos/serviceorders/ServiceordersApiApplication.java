b9ec9ead61e0105150c920f6dc864b7602db3678
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2020-08-17 19:39:57 -0300

    Create Spring project

---
+++ src/main/java/com/marcos/serviceorders/ServiceordersApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.serviceorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceordersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceordersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/serviceorders/ServiceordersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.serviceorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceordersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
